b82a2666f94ceb9ff3d646d0aab860dc7af7dcb1
Adarsh Dash <17adarshdash@gmail.com>
2026-06-28 15:46:23 +0530

    Day 37: Flask routes returning JSON

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Hello World"
+
+@app.route('/about')
+def about():
+    return jsonify({"name": "Adarsh", "role": "Student"})
+
+@app.route('/fake')
+def fake():
+    tasks = [
+        {"task": "Gym", "completed": "yes"},
+        {"task": "Office", "completed": "no"},
+        {"task": "Home", "completed": "no"}
+    ]
+    return jsonify(tasks)
 if __name__ == '__main__':
     app.run()
